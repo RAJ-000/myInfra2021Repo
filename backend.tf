ac8e457815b8a8a173d4db3c234da7d5bc54706e
RAJ-000 <95397695+RAJ-000@users.noreply.github.com>
2022-02-11 12:01:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "my-bucket-raj2"
     key = "main"
-    region = "us-east-2"
+    region = "us-west-2"
     dynamodb_table = "my-dynamodb-table"
   }
 }
